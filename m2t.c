916535ecab98144e11ffdbcd7ae48707c61d25c1
Natan Felipe <contato@natanfelipe.com.br>
2025-05-19 13:52:39 +0000

    refactor: changed parameter name to "value" for code standardization

--- m2t.c
+++ m2t.c
@@ -15,31 +15,31 @@ typedef struct
     int i;
 } SplitInGroupsOfThree;
 
-MoneySplit money_splt_in_two_halfs(char *money) // recebe o valor "5432178,99"
+MoneySplit money_splt_in_two_halfs(char *value) // recebe o valor "5432178,99"
 {
     MoneySplit money_split;
-    char *separator = strchr(money, ','); // identifica o separador retornando ",99"
+    char *separator = strchr(value, ','); // identifica o separador retornando ",99"
 
     if (separator != NULL)
     {
-        int char_amount = separator - money;             // retorna a quantidade de caracteres que vem antes da virgula/ponto
-        strncpy(money_split.l_half, money, char_amount); // copia para dentro de "l_half" o valor "5432178" que vem antes da virgula
+        int char_amount = separator - value;             // retorna a quantidade de caracteres que vem antes da virgula/ponto
+        strncpy(money_split.l_half, value, char_amount); // copia para dentro de "l_half" o valor "5432178" que vem antes da virgula
         money_split.l_half[char_amount] = '\0';          // adiciona ao final do array de strings o caracter '\0' para indicar o fim da string
         strcpy(money_split.r_half, separator + 1);       // copia para dentro de "r_half" o valor "99" que vem depois da virgula
     }
     else
     {
-        strcpy(money_split.l_half, money); // Se não tiver separador copia o valor de "money" para dentro de "l_half"
+        strcpy(money_split.l_half, value); // Se não tiver separador copia o valor de "value" para dentro de "l_half"
         strcpy(money_split.r_half, "00");  // Se não tiver separador adiciona o valor "00" depois da virgula
     }
 
     return money_split;
 }
 
-SplitInGroupsOfThree split_in_groups_of_three(char *data)
+SplitInGroupsOfThree split_in_groups_of_three(char *value)
 {
     SplitInGroupsOfThree sigot;
-    sigot.len = strlen(data); // Descobre quantos caracteres tem na string
+    sigot.len = strlen(value); // Descobre quantos caracteres tem na string
     sigot.group_index = 0;    // Contador de quantos grupos já salvei
     sigot.i = sigot.len;      // Começamos do fim da string
 
@@ -50,7 +50,7 @@ SplitInGroupsOfThree split_in_groups_of_three(char *data)
             start = 0; // Se passar do início da string, ajusta
 
         int size = sigot.i - start;                                   // Quantos caracteres o grupo vai ter (1, 2 ou 3)
-        strncpy(sigot.groups[sigot.group_index], &data[start], size); // Copia o trecho do grupo
+        strncpy(sigot.groups[sigot.group_index], &value[start], size); // Copia o trecho do grupo
         sigot.groups[sigot.group_index][size] = '\0';                 // Termina a string com \0
 
         sigot.group_index++; // Próximo grupo
