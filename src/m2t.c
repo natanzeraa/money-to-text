f00e621b6f29d0cbed5570e159d444c71a9d8877
Natan Felipe <contato@natanfelipe.com.br>
2025-05-21 18:40:39 +0000

    refactor: removed main function from m2t.c

--- src/m2t.c
+++ src/m2t.c
@@ -195,15 +195,3 @@ void identify_value_class(SplitInGroupsOfThree sigot)
 
     printf("\n");
 }
-
-// int main()
-// {
-//     MoneySplit split = money_splt_in_two_halfs("10,01");
-
-//     if (atoi(split.l_half) == 0 && atoi(split.r_half) == 0)
-//         printf("Nenhum valor foi fornecido\n");
-
-//     SplitInGroupsOfThree sigot = split_in_groups_of_three(split.l_half);
-//     identify_value_class(sigot);
-//     return 0;
-// }
