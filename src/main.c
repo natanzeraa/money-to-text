fbcee6e2cf9015e168e9421649cbcdfe3823ba0c
natanzeraa <natanzeraaaaaa@gmail.com>
2025-05-22 20:44:34 -0300

    refactor: upodate main function to adapt the new structure

--- src/main.c
+++ src/main.c
@@ -4,12 +4,25 @@
 
 int main()
 {
-    MoneySplit split = money_splt_in_two_halfs("44240,01");
+    MoneySplit split = money_splt_in_two_halfs("45240,01");
 
     if (atoi(split.l_half) == 0 && atoi(split.r_half) == 0)
         printf("Nenhum valor foi fornecido\n");
 
-    SplitInGroupsOfThree sigot = split_in_groups_of_three(split.l_half);
-    identify_value_class(sigot);
+    SplitInGroupsOfThree split_groups = split_in_groups_of_three(split.l_half);
+
+    int result_count;
+    ValueClassResult *results = identify_value_class(split_groups, &result_count);
+
+    for (int i = 0; i < result_count; i++)
+    {
+        char *text = translate_value_to_txt(&results[i]);
+        printf("%s", text);
+        free(text);
+    }
+
+    printf("\n");
+    free(results);
+
     return 0;
 }
