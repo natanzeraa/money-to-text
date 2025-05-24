9da5f9dc38e3998e53ba207d4ed8b439e63abbb9
natanzeraa <natanzeraaaaaa@gmail.com>
2025-05-24 11:45:22 -0300

    feat: separate translation of left and right values for more flexible formatting

--- src/main.c
+++ src/main.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../include/m2t.h"
 
 int main()
 {
-    MoneySplit split = money_splt_in_two_halfs("45240,01");
+    MoneySplit split = money_splt_in_two_halfs("45240,45");
 
-    if (atoi(split.l_half) == 0 && atoi(split.r_half) == 0)
+    if (atoi(split.left_value) == 0 && atoi(split.right_value) == 0)
         printf("Nenhum valor foi fornecido\n");
 
-    SplitInGroupsOfThree split_groups = split_in_groups_of_three(split.l_half);
+    SplitInGroupsOfThree split_groups = split_in_groups_of_three(split.left_value);
 
     int result_count;
     ValueClassResult *results = identify_value_class(split_groups, &result_count);
 
     for (int i = 0; i < result_count; i++)
     {
-        char *text = translate_value_to_txt(&results[i]);
-        printf("%s", text);
-        free(text);
+        char *real = translate_left_value_to_txt(&results[i]);
+        printf("%s", real);
+        free(real);
+    }
+
+    for (int i = 0; i < 1; i++)
+    {
+        char *cents = translate_right_value_to_txt(split);
+        printf("%s", cents);
+        free(cents);
     }
 
     printf("\n");
